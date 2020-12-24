a3bd0c2e47e67f5d7a5b45fdaa3efd1db086f83b
rennanmalves <76570599+rennanmalves@users.noreply.github.com>
2020-12-24 20:44:01 -0300

    Add files via upload

---
+++ Fun_categorias.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Fun_categorias {
+    private $idcategorias;
+    private $idfuncionario;
+    function getIdcategorias() {
+        return $this->idcategorias;
+    }
+
+    function getIdfuncionario() {
+        return $this->idfuncionario;
+    }
+
+    function setIdcategorias($idcategorias) {
+        $this->idcategorias = $idcategorias;
+    }
+
+    function setIdfuncionario($idfuncionario) {
+        $this->idfuncionario = $idfuncionario;
+    }
+
+
+}
---
+++ Fun_serviço.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Fun_serviço {
+    private $idserviço;
+    private $idfuncionario;
+    function getIdserviço() {
+        return $this->idserviço;
+    }
+
+    function getIdfuncionario() {
+        return $this->idfuncionario;
+    }
+
+    function setIdserviço($idserviço) {
+        $this->idserviço = $idserviço;
+    }
+
+    function setIdfuncionario($idfuncionario) {
+        $this->idfuncionario = $idfuncionario;
+    }
+
+
+            
+}
